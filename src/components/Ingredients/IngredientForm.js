49e90c76d64ffea02fa11ecacb47a0978b4b6832
Khaibar Saadat <khybersaadat93@gmail.com>
2023-07-22 17:54:43 +0200

    Implement Two-Way Binding with Single useState for Real-Time User Input Storage in IngredientForm.js

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../UI/Card";
 import "./IngredientForm.css";
 
 const IngredientForm = React.memo((props) => {
+  const [inputState, setInputState] = useState({ title: "", amount: "" });
+  console.log(inputState);
+
   const submitHandler = (event) => {
     event.preventDefault();
     // ...
+  };
+
+  const titleChangeHandler = (event) => {
+    const newTitle = event.target.value;
+
+    setInputState((prevInputState) => ({
+      title: newTitle,
+      amount: inputState.amount,
+    }));
+  };
+
+  const amountChangeHandler = (event) => {
+    const newAmount = event.target.value;
+
+    setInputState((prevInputState) => ({
+      title: prevInputState.title,
+      amount: newAmount,
+    }));
   };
 
   return (
@@ -15,11 +36,21 @@ const IngredientForm = React.memo((props) => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+            <input
+              type="text"
+              id="title"
+              value={inputState.title}
+              onChange={titleChangeHandler}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+            <input
+              type="number"
+              id="amount"
+              value={inputState.amount}
+              onChange={amountChangeHandler}
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
