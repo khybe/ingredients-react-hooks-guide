8b5525ae74aac8816f92c99db77323c812c1582a
Khaibar Saadat <khybersaadat93@gmail.com>
2023-07-23 16:08:49 +0200

    Submitting the IngredientForm and passing its State Data Across Components

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -10,7 +10,10 @@ const IngredientForm = React.memo((props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
+
+    setEnteredTitle("");
+    setEnteredAmount("");
   };
 
   const titleChangeHandler = (event) => setEnteredTitle(event.target.value);
