f9f5eed11f54ee8cfa804878be9a86d970b36d6e
Khaibar Saadat <khybersaadat93@gmail.com>
2023-07-29 17:03:15 +0200

    Working with useContext() for User Authentication

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import App from "./App";
+import AuthContextProvider from "./context/auth-context";
+import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+);
